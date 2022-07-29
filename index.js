4db23521c199328f5be7fcded09c4f5c46cb4f8a
Nimrus <59753998+Nimrus@users.noreply.github.com>
2022-07-29 19:52:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//“JavaScript-код с созданием переменных”
+/* 
+“Автор:”
+Nimrus
+*/
+let myName = Jone;
+console.log (myName);
+let mySurname = Peshkov;
+console.log (mySurname);
+let myFavoriteDrink = Voter;
+console.log (myFavoriteDrink);
+const myFavoriteAnimal = Cat;
+console.log (myFavoriteAnimal);
+const (myFavoriteProgrammingLanguage);
+console.log (myFavoriteProgrammingLanguage);
